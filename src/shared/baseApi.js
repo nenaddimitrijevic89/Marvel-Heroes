89d3162b0ea44da9a9eb5415060b9b25718a21e0
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-07-20 19:10:27 +0200

    deploy marvel heroes on github

--- src/shared/baseApi.js
+++ src/shared/baseApi.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const baseAPI = Axios.create({
-    baseURL: 'http://gateway.marvel.com/v1/public'
+    baseURL: 'https://gateway.marvel.com/v1/public'
 })
 
 const key = '65fafb75e2e947221eb1e1f007bc775f';
