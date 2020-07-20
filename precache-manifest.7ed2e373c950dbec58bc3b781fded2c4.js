1a9187f170896aeb7615372325aa190898f4be25
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-07-20 18:45:41 +0200

    Updates

---
+++ precache-manifest.7ed2e373c950dbec58bc3b781fded2c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d2959e401c47fbad6d2ce7646803af",
+    "url": "/Marvel-Heroes/index.html"
+  },
+  {
+    "revision": "6c9b11368bd5ed4ff295",
+    "url": "/Marvel-Heroes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "feb49dfa40f9bd7f6499",
+    "url": "/Marvel-Heroes/static/css/main.48925722.chunk.css"
+  },
+  {
+    "revision": "6c9b11368bd5ed4ff295",
+    "url": "/Marvel-Heroes/static/js/2.480664f3.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/Marvel-Heroes/static/js/2.480664f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feb49dfa40f9bd7f6499",
+    "url": "/Marvel-Heroes/static/js/main.bd3f33fd.chunk.js"
+  },
+  {
+    "revision": "a3f86c509100081b0430",
+    "url": "/Marvel-Heroes/static/js/runtime-main.7f5ad179.js"
+  }
+]);
